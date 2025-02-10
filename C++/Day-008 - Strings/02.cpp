50abf98f28d80224ed8bcb9791c69d42305c5050
adhyan-jain <adhyanjain2006@gmail.com>
2025-02-10 21:02:31 +0530

    c++ day-8

---
+++ C++/Day-008 - Strings/02.cpp
@@ -0,0 +1,28 @@
+// 2. Create a program to check if a string is a palindrome.
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void func2(string str) {
+    string rev1 = str;
+    reverse(rev1.begin(), rev1.end());
+    cout << "By function: " << (str == rev1 ? "Yes" : "No") << endl;
+
+    bool isPalindrome = true;
+    int n = str.length();
+    for (int i = 0; i < n / 2; i++) {
+        if (str[i] != str[n - i - 1]) {
+            isPalindrome = false;
+            break;
+        }
+    }
+    cout << "By normal method: " << (isPalindrome ? "Yes" : "No") << endl;
+}
+
+int main() {
+    string str;
+    cout << "Enter a string: ";
+    getline(cin, str);
+    func2(str);
+    return 0;
+}
---
+++ C++/Day-008 - Strings/03.cpp
@@ -0,0 +1,35 @@
+// 3. Implement a program to count the number of vowels and consonants in a string.
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void func3(string str) {
+    int v1 = 0, c1 = 0;
+    string vowels = "aeiouAEIOU";
+
+    for (char ch : str) {
+        if (isalpha(ch)) {
+            if (vowels.find(ch) != string::npos) v1++;
+            else c1++;
+        }
+    }
+    cout << "By function: Vowels = " << v1 << ", Consonants = " << c1 << endl;
+
+    int v2 = 0, c2 = 0;
+    for (char ch : str) {
+        ch = tolower(ch);
+        if (isalpha(ch)) {
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') v2++;
+            else c2++;
+        }
+    }
+    cout << "By normal method: Vowels = " << v2 << ", Consonants = " << c2 << endl;
+}
+
+int main() {
+    string str;
+    cout << "Enter a string: ";
+    getline(cin, str);
+    func3(str);
+    return 0;
+}
