302a9263dd0da0cb9403fb3d3a5a5a5c8cfeed63
adhyan-jain <adhyanjain2006@gmail.com>
2025-02-17 22:15:16 +0530

    Day-14

---
+++ C++/Day-014 - Nested membership functions/01.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Here the chk_bin function is set as provate and can only be accessed by the function of binary class
+// chk_bin function is used inside the ones_compliment function which is also called nesting of membership functions
+
+class binary
+{
+private:
+    string s;
+    void chk_bin(void);
+
+public:
+    void read(void);
+    void ones_compliment(void);
+    void display(void);
+};
+
+void binary::read(void)
+{
+    cout << "Enter a binary number" << endl;
+    cin >> s;
+}
+
+void binary::chk_bin(void)
+{
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s.at(i) != '0' && s.at(i) != '1')
+        {
+            cout << "Incorrect binary format" << endl;
+            exit(0);
+        }
+    }
+}
+
+void binary::ones_compliment(void)
+{
+    chk_bin();
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s.at(i) == '0')
+        {
+            s.at(i) = '1';
+        }
+       else
+        {
+            s.at(i) = '0';
+        }
+    }
+}
+
+void binary::display(void)
+{
+    cout<<"Displaying your binary number"<<endl;
+    for (int i = 0; i < s.length(); i++)
+    {
+        cout << s.at(i);
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    binary b;
+    b.read();
+    // b.chk_bin();
+    b.display();
+    b.ones_compliment();
+    b.display();
+
+    return 0;
+}
