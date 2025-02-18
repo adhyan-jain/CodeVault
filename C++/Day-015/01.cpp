41ab8df0c97453aeaf296b09eafc5c74b16d29d9
adhyan-jain <adhyanjain2006@gmail.com>
2025-02-18 23:21:10 +0530

    Update day-15

--- C++/Day-015/01.cpp
+++ C++/Day-015/01.cpp
@@ -1,1 +1,45 @@
 #include <iostream>
+using namespace std;
+
+// in normal functions it takes more memory since we create a new copy of a variable everytime we need to use them
+// In classes we do not need a seperate copy for evry function because it can share one copy
+
+class Shop
+{
+    int itemId[100];
+    int itemPrice[100];
+    int counter;
+
+public:
+    void initCounter(void) { counter = 0; }
+    void setPrice(void);
+    void displayPrice(void);
+};
+
+void Shop ::setPrice(void)
+{
+    cout << "Enter Id of your item no " << counter + 1 << endl;
+    cin >> itemId[counter];
+    cout << "Enter Price of your item" << endl;
+    cin >> itemPrice[counter];
+    counter++;
+}
+
+void Shop ::displayPrice(void)
+{
+    for (int i = 0; i < counter; i++)
+    {
+        cout << "The Price of item with Id " << itemId[i] << " is " << itemPrice[i] << endl;
+    }
+}
+
+int main()
+{
+    Shop dukaan;
+    dukaan.initCounter();
+    dukaan.setPrice();
+    dukaan.setPrice();
+    dukaan.setPrice();
+    dukaan.displayPrice();
+    return 0;
+}
