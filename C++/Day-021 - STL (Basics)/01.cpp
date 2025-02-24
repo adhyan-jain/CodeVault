f5f7eaa846cca76c400d8f95a3531e031ab9a248
adhyan-jain <adhyanjain2006@gmail.com>
2025-02-24 18:16:06 +0530

    day-21

--- C++/Day-021 - STL (Basics)/01.cpp
+++ C++/Day-021 - STL (Basics)/01.cpp
@@ -11,16 +11,39 @@ Merge both the arrays
 #include <iostream>
 using namespace std;
 
-void mergesort(int begin, int end, int arr[]){
+void mergeSort(int arr[], int leftEnd, int rightEnd, int temp[]) {
+    int mid = leftEnd + (rightEnd - leftEnd) / 2;
+    int left = leftEnd, right = mid + 1, k = leftEnd;
     
-    if(begin>=end){}
-    return
+    while (left <= mid && right <= rightEnd) {
+        temp[k++] = (arr[left] <= arr[right]) ? arr[left++] : arr[right++];
+    }
+    while (left <= mid) temp[k++] = arr[left++];
+    while (right <= rightEnd) temp[k++] = arr[right++];
+    
+    for (int i = leftEnd; i <= rightEnd; i++) {
+        arr[i] = temp[i];
+    }
 }
 
-int merge(){}
+void merge(int arr[], int leftEnd, int rightEnd, int temp[]) {
+    if (leftEnd >= rightEnd) return;
+    int mid = leftEnd + (rightEnd - leftEnd) / 2;
+    
+    merge(arr, leftEnd, mid, temp);
+    merge(arr, mid + 1, rightEnd, temp);
+    mergeSort(arr, leftEnd, rightEnd, temp);
+}
 
-int main(){
+int main() {
     int arr[] = {3, 9, 6, 1};
+    int n = 4;
+    int temp[n];
+    
+    merge(arr, 0, n - 1, temp);
+    
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
     
     return 0;
 }
