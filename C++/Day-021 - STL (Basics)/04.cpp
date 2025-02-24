1842b7e0767f78958bc507f349823464b7302b26
adhyan-jain <adhyanjain2006@gmail.com>
2025-02-24 16:28:45 +0530

    day-21

---
+++ C++/Day-021 - STL (Basics)/04.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // Initialize vector
+    vector<int> v = {1, 3, 5, 6};
+    v.push_back(5);  // adds the element in parenthesis at the end of the array
+    
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+    
+    random_shuffle(v.rbegin(), v.rend()); // Shuffles the array randommly
+    auto r = *(max_element(v.begin(), v.end()));
+    cout << r << endl;
+    
+    int limit = 1;
+    v.erase(v.begin() + 1);
+    v.insert(v.begin(), 4, 3);
+    
+    // Shorter Way
+    for (auto x : v)
+    {
+        cout << x << " ";
+    }
+    
+    vector<vector<int>> v1;  // Nested vector
+    v1.push_back({41, 4, 15, 6});
+    v1.push_back({10, 14, 6, 7, 8, 9});
+    
+    for (auto x : v1)
+    {
+        for (auto y : x)
+        {
+            cout << y << " ";
+        }
+        cout << endl;
+    }
+    
+    for (auto &j : v1)
+    {
+        sort(j.begin(), j.end());
+    }
+    
+    cout << "Sorted" << endl;
+    for (auto x : v1)
+    {
+        for (auto y : x)
+        {
+            cout << y << " ";
+        }
+        cout << endl;
+    }
+}
