bde9ce58c36ab8c2d568375385f3bc8ff9149c7e
adhyan-jain <adhyanjain2006@gmail.com>
2025-02-25 18:30:00 +0530

    Day-22 c++

---
+++ C++/Day-022 Pointers Revision/06.cpp
@@ -0,0 +1,48 @@
+/*
+
+Problem Statement
+
+
+
+You are tasked with creating a program to calculate the area of a rectangle. You need to implement this using pointer arithmetic. 
+
+
+
+Write a program that takes the length and breadth as input, calculates the area using pointer arithmetic, and then displays the result.
+
+Input format :
+The input consists of two space-separated integers representing the length and breadth of a rectangle.
+
+Output format :
+The output prints an integer representing the area of the rectangle.
+
+
+
+Refer to the sample output for formatting specifications.
+
+Code constraints :
+1 ≤ length, breadth ≤ 1000
+
+Sample test cases :
+Input 1 :
+2 4
+Output 1 :
+8
+Input 2 :
+15 9
+Output 2 :
+135
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int length, breadth;
+    int* ptr1 = &length;
+    int* ptr2 = &breadth;
+    cin>>*ptr1>>*ptr2;
+    cout<<(*ptr1)*(*ptr2);
+    return 0;
+}
