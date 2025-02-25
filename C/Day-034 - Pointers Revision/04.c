d08c74f2b2c771ffcfb16a376af5fb5e9353a7d8
adhyan-jain <adhyanjain2006@gmail.com>
2025-02-25 18:30:00 +0530

    Day-22 c++

---
+++ C/Day-034 - Pointers Revision/04.c
@@ -0,0 +1,61 @@
+/*
+
+
+Problem Statement
+
+
+
+Anuj is working on a program to find the largest of two numbers by passing the addresses of the integers to the function.
+
+
+
+Function Signature: int* findLarger(int*, int*)
+
+Input format :
+The first line of input consists of an integer n, representing the first number.
+
+The second line of input consists of an integer n1, representing the second number.
+
+Output format :
+The output prints the largest number of the given two numbers.
+
+
+
+Refer to the sample output for the formatting specifications.
+
+Code constraints :
+-106 ≤ numbers ≤ 106
+
+Sample test cases :
+Input 1 :
+58 69
+Output 1 :
+69
+Input 2 :
+-856 -987
+Output 2 :
+-856
+
+*/
+
+#include <stdio.h>
+
+int* findLarger(int* ptr1, int* ptr2){
+    if(*ptr1>*ptr2){
+        return ptr1;
+    }
+    else{
+        return ptr2;
+    }
+}
+
+int main(){
+    int num1, num2;
+    scanf("%d", &num1);
+    scanf("%d", &num2);
+    int* ptr1=&num1;
+    int* ptr2=&num2;
+    int* ans= findLarger(ptr1, ptr2);
+    printf("%d", *ans);
+    return 0;
+}
