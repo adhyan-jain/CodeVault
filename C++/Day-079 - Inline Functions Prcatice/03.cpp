d0be4b42fb9f9c74e10bb04e0e82500783cdbecc
adhyan-jain <adhyanjain2006@gmail.com>
2025-03-20 12:00:00 +0530

    day-79

---
+++ C++/Day-079 - Inline Functions Prcatice/03.cpp
@@ -0,0 +1,26 @@
+/*
+Problem Statement
+
+Write a program that asks the user to enter two integers and then calculates the greatest common 
+divisor (GCD) of the two numbers using an inline function. The GCD can be calculated using Euclid's 
+algorithm.
+*/
+
+#include <iostream>
+using namespace std;
+inline int gcd(int a, int b) {
+    if (b == 0) {
+        return a;
+    } else {
+        return gcd(b, a % b);
+    }
+}
+
+int main() {
+    int num1, num2;
+    // cout << "Enter two integers: ";
+    cin >> num1 >> num2;
+    int result = gcd(num1, num2);
+    cout << "GCD of " << num1 << " and " << num2 << " is " << result << std::endl;
+    return 0;
+}
