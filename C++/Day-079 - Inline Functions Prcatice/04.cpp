c0e056b2bca5b7673f3a9cf6a768343b70a308e0
adhyan-jain <adhyanjain2006@gmail.com>
2025-03-20 12:00:00 +0530

    day-79

---
+++ C++/Day-079 - Inline Functions Prcatice/04.cpp
@@ -0,0 +1,65 @@
+/*
+Problem Statement
+
+You have been assigned a task to create a program that performs arithmetic operations using the Operation 
+class and using inline functions.
+
+You have declared the following inline functions within the class: get(), sum(), difference(), product(), 
+division(), and modulus(). These functions are used to perform various arithmetic operations. Implement 
+the class and its methods as described above.
+
+Then, create an object of the Operation class and demonstrate how the methods can be called to perform 
+the necessary arithmetic calculations. Provide the complete code solution for the program, including 
+the class definition, method implementations, and the main method where the object is created and the
+ methods are called.
+
+Note: This is a sample question asked in a TCS interview.
+*/
+
+#include <iostream>
+using namespace std;
+class Operation{
+    int a,b;
+public:
+    void get();
+    void sum();
+    void difference();
+    void product();
+    void division();
+    void modulus();
+};
+
+inline void Operation :: get(){
+    cin >> a;
+    cin >> b;
+}
+
+inline void Operation :: sum(){
+    cout << "Addition of two numbers: " << a+b << "\n";
+}
+
+inline void Operation :: difference(){
+    cout << "Difference of two numbers: " << a-b << "\n";
+}
+
+inline void Operation :: product(){
+    cout << "Product of two numbers: " << a*b << "\n";
+}
+
+inline void Operation ::division(){
+    cout<<"Division of two numbers: "<<a/b<<"\n" ;
+}
+inline void Operation ::modulus(){
+    cout<<"Modulus of two numbers: "<<a%b ;
+}
+
+int main(){
+    Operation s;
+    s.get();
+    s.sum();
+    s.difference();
+    s.product();
+    s.division();
+    s.modulus();
+    return 0;
+}
