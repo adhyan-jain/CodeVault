1f6081864d35e56135d5b87ce7fd1ed00b4a25df
adhyan-jain <adhyanjain2006@gmail.com>
2025-03-27 12:00:00 +0530

    day-86

---
+++ C++/Day-086/02.cpp
@@ -0,0 +1,107 @@
+/*
+Problem Statement
+
+
+
+Arvind is designing a program to calculate electrical power and efficiency, and he needs your help. Create a base class P with voltage and current attributes to compute power. Implement a derived class PE with an efficiency attribute to determine useful and wasted power.
+
+
+
+Write a program to help him accomplish the task.
+
+
+
+Formulas
+
+Power = Voltage * Current
+
+Useful Power = Power * Efficiency
+
+Wasted Power = Power * (1.0 - Efficiency)
+
+Input format :
+The input consists of three space-separated double values, representing the voltage, current, and efficiency.
+
+Output format :
+The first line of output prints the calculated electrical power in watts (W).
+
+The second line prints the useful electrical power in watts (W) after considering efficiency.
+
+The third line prints the wasted electrical power in watts (W) due to inefficiency.
+
+All the values are double values that are rounded to one decimal place.
+
+
+
+Refer to the sample output for formatting specifications.
+
+Code constraints :
+In this scenario, the test cases fall under the following constraints:
+
+1.0 ≤ voltage ≤ 500.0
+
+1.0 ≤ current ≤ 50.0
+
+0.01 ≤ efficiency ≤ 0.99
+
+Sample test cases :
+Input 1 :
+120.7 5.0 0.85
+Output 1 :
+Power: 603.5W
+Useful Output Power: 513.0W
+Wasted Power: 90.5W
+Input 2 :
+347.8 23.6 0.47
+Output 2 :
+Power: 8208.1W
+Useful Output Power: 3857.8W
+Wasted Power: 4350.3W
+*/
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+class P{
+    protected:
+        double voltage;
+        double current;
+    public:
+    P(double a, double b) : voltage(a), current(b){}
+};
+
+class PE : public P{
+    protected:
+        double efficiency;
+    public:
+        PE(double a, double b, double c) : P(a,b), efficiency(c){}
+        
+        double power(){
+            return voltage * current;
+        }
+        
+        void UsefulPower(){
+            cout << "Useful Output Power: " << fixed << setprecision(1) << power() * efficiency << "W" <<endl;
+        }
+        
+        void WastefulPower(){
+            cout << "Wasted Power: " << fixed << setprecision(1) << power() * (1-efficiency) << "W" <<endl;
+        }
+        
+        void display(){
+            cout <<"Power: " << fixed << setprecision(1) << power() << "W" <<endl;
+            UsefulPower();
+            WastefulPower();
+        }
+};
+
+int main(){
+    double a, b, c;
+    cin>>a>>b>>c;
+    
+    PE p(a, b, c);
+    p.display();
+    return 0;
+}
