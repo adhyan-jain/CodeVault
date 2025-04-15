8802781cdec3cede0bdb0b1dd015c919a8d07f3d
adhyan-jain <adhyanjain2006@gmail.com>
2025-04-15 12:00:00 +0530

    Day-105

---
+++ C++/Day-105/01.cpp
@@ -0,0 +1,80 @@
+/*
+Problem Statement
+
+
+
+Sharon is working on a population growth analysis program. She is studying how populations change over time. 
+
+
+
+She wants to write a program that calculates the growth rate of a population based on its initial and final sizes.
+
+
+
+Help Sharon write a program that includes a base class Popul and a derived class Birth. Popul has a virtual function calcRate() to initialize the initial and final sizes. Class Birth inherits from Popul, overriding calcRate() to calculate the growth rate based on births.
+
+
+
+Note: Growth Rate = (1000 * (final polulation - initial population) / initial population)
+
+Input format :
+The first line of input consists of an integer N, representing the initial population.
+
+The second line consists of an integer P, representing the final population.
+
+Output format :
+The output prints a double value, representing the population growth rate, rounded off to two decimal places.
+
+
+
+Refer to the sample output for formatting specifications.
+
+Code constraints :
+In this scenario, the test cases fall under the following constraints:
+
+10 ≤ N ≤ 107
+
+10 ≤ P ≤ 107
+
+Sample test cases :
+Input 1 :
+100
+1000
+Output 1 :
+9000.00
+Input 2 :
+456
+768
+Output 2 :
+684.21
+
+*/
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+ 
+ 
+class Popul {
+    public:
+    virtual void calcRate() {};
+};
+
+class Birth: public Popul {
+    public:
+    int n, p;
+    Birth(int n, int p): n(n), p(p) {}
+    void calcRate() override {
+        double total = (p-n)/static_cast<double>(n);
+        cout << fixed << setprecision(2) << 1000 * total << endl;
+    }
+};
+
+int main() {
+    int n, p; 
+    cin >> n >> p;
+    Popul *ptr; 
+    Birth obj(n, p);
+    ptr = &obj;
+    ptr -> calcRate();
+}
